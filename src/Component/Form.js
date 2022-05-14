151d3c8a654177957d4f510c121bbc4d86e6d83d
Quentin Alais <quentin.alais@edu.devinci.fr>
2022-05-14 18:56:56 +0100

    update

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -5,7 +5,7 @@ const Form = () => {
     <div className='px-5 py-5'>
         <div class="card" >
             <div class="card-body">
-                <h5 class="card-title">Welcome to Bonzai</h5>
+                <h5 class="card-title">Hola from Bonzai</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
