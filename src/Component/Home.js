8067a9ffecc9793186848f12afe5a2ed3a71c6fd
Quentin Alais <quentin.alais@edu.devinci.fr>
2022-05-27 23:58:51 +0100

    Add Attestation Form

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,24 +4,17 @@ import bonzai_pic from '../Images/MyBonzai.png';
 export const Home = () => {
   return (
     <main className="px-5 py-3 ">
-    <div class="row featurette py-3 ">
-      <div class="col-md-6">
-        <h2 class="featurette-heading fw-normal lh-1">Welcome to the <span class="text-muted">Bonzai project.</span></h2>
-        <p class="lead">Try not. Do. Or do not. </p>
-        <p class="lead">There is no try.  </p>
+      <div class="row featurette py-3 ">
+        <div class="col-md-6">
+          <h2 class="featurette-heading fw-normal lh-1">Welcome to the <span class="text-muted">Bonzai project.</span></h2>
+          <p class="lead">Try not. Do. Or do not. </p>
+          <p class="lead">There is no try.  </p>
+        </div>
+        <div class="col-md-5">
+          <img class="rounded float-start" width="300" height="400" src={bonzai_pic} alt="" />
+        </div>
       </div>
-      <div class="col-md-5">
-        <img class="rounded float-start" width="300" height="400" src={bonzai_pic} alt="" />
-
-      </div>
-    </div>
-
-
-
-
-    
-
-</main>
+  </main>
         
   )
 }
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -3,7 +3,14 @@ import React from 'react'
 export const Dashboard = () => {
   return (
     <div>
-        <p> Hello World from Dashboard. </p>
+        <div class="card border-dark">
+          <div class="card-header">
+            <b>Dashboard</b>
+          </div>
+          <div class="card-body">
+            Welcome to the dashboard section.
+          </div>
+        </div>
     </div>
   )
 }
