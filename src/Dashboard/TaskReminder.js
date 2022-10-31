94818d52d15585fdf8419e86068a386fa603c094
Quentin Alais <quentin.alais@edu.devinci.fr>
2022-10-31 20:41:59 +0000

    Add Delete Functionality

--- src/Dashboard/TaskReminder.js
+++ src/Dashboard/TaskReminder.js
@@ -3,6 +3,8 @@ import { Grid, GridItem, Button} from "@chakra-ui/react";
 import ModalTask from "./ModalTask";
 import { AiOutlineDelete , AiOutlineEdit} from 'react-icons/ai';
 import useLocalStorage  from "../hooks/useLocalStorage";
+import { v4 as uuidv4 } from 'uuid';
+
 
 import {
     List,
@@ -21,7 +23,7 @@ function TaskReminder() {
   const [data, setdata] = useLocalStorage('data',[])  
 
   const addTask =(task_name)=>{
-    var current = {name : task_name}
+    var current = {name : task_name, id: uuidv4()}
     if(data){
         var data_copy = [...data]
         data_copy.push(current)
@@ -30,6 +32,10 @@ function TaskReminder() {
     else{
         setdata([current])
     }
+  }
+
+  const deleteTask = (task_id) =>{
+    setdata(data.filter(item=>item.id !== task_id))
   }
 
   return (
@@ -52,7 +58,7 @@ function TaskReminder() {
         <GridItem pl="2"  area={"nav"}>
           Tasks
           <br/>
-          <MyTasks data={data}/>
+          <MyTasks data={data} deleteTask={deleteTask} />
 
         </GridItem>
         <GridItem pl="2"  area={"main"}>
@@ -69,7 +75,7 @@ function TaskReminder() {
 export default TaskReminder;
 
 
-const MyTasks = ({data}) => {
+const MyTasks = ({data, deleteTask }) => {
     if( data === null )return <div></div>
     if( data === [] )return <div></div>
 
@@ -78,7 +84,7 @@ const MyTasks = ({data}) => {
             <UnorderedList style={{listStyle:'none'}}>
             {data.map((task,id)=>{
                 return(
-                    <ListItem key={id}><MyBox text={task.name}/></ListItem>
+                    <ListItem key={id}><MyBox task = {task} deleteTask={deleteTask}/></ListItem>
                 )
             })}
         </UnorderedList>
@@ -87,35 +93,40 @@ const MyTasks = ({data}) => {
 }
 
 
-const MyBox = ({ text }) => {
+const MyBox = ({ task,deleteTask}) => {
   return (
     <Box
       borderWidth="2px"
       borderRadius="lg"
-      bg="orange"
+      bg="blue.600"
       w="100%"
       p={4}
       color="white"
     >
       <Flex color="white">
-        <Center w="200px" >
-          <Text>{text}</Text>
+        <Center w="200px">
+          <Text>{task.name}</Text>
         </Center>
-        <Center flex="1" >
-          <Text><AiOutlineEdit 
-                    size="20px"
-                    onMouseOver={({target})=>target.style.color="black"} 
-                    onMouseOut={({target})=>target.style.color="white"}
-          /></Text>
+        <Center flex="1">
+          <div className="container flex mycontainer" onClick={() => alert("Edit")}>
+            <AiOutlineEdit
+              size="20px"
+              onMouseOver={({ target }) => (target.style.color = "black")}
+              onMouseOut={({ target }) => (target.style.color = "white")}
+            />
+          </div>
+            
         </Center>
-        <Center flex="1" >
-          <AiOutlineDelete 
-          size="20px"
-          onMouseOver={({target})=>target.style.color="black"} 
-          onMouseOut={({target})=>target.style.color="white"}
-          />
+        <Center flex="1">
+            <div className="container flex mycontainer" onClick={() => deleteTask(task.id)}>
+              <AiOutlineDelete
+                className="justify-content-center"
+                size="20px"
+                onMouseOver={({ target }) => (target.style.color = "black")}
+                onMouseOut={({ target }) => (target.style.color = "white")}
+              />
+            </div>
         </Center>
-        
       </Flex>
     </Box>
   );
