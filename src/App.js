4e1bafcd672b9706643ed14ddedef1b19951b320
Quentin Alais <quentin.alais@edu.devinci.fr>
2022-11-04 00:36:37 +0000

    Change Template to Chakra Template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
 import "./App.css";
-import Header from "./Component/Header";
-import Footer from "./Component/Footer";
 import {
   BrowserRouter ,
   Routes,
@@ -10,22 +8,22 @@ import {
 import { Home } from "./Component/Home";
 import { Dashboard } from "./Dashboard/Dashboard";
 import { ChakraProvider } from '@chakra-ui/react'
+import SimpleSidebar from "./Component/Sidebar";
 
 
 function App() {
   return (
-<ChakraProvider>
-    <BrowserRouter>
-      <Header></Header>
-      <div class="mx-5 py-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-
-      <Footer></Footer>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        {/* <Header></Header> */}
+        <SimpleSidebar >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Explore" element={<Dashboard />} />
+          </Routes>
+        </SimpleSidebar>
+        {/* <Footer></Footer> */}
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
